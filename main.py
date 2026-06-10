d07fa50006563923f1467ed5d1f4230b1f4aa8a2
Dking08 <dastageer.siddiqui@gmail.com>
2026-06-10 18:40:54 +0530

    fix: Delete broken test main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-﻿from fastapi import FastAPI
-from graph_routes import graph_router
-
-app = FastAPI(title="Graph Test")
-app.include_router(graph_router, prefix="/api/graph", tags=["Microsoft Graph"])
